52e46d06475728bc68b54f48ccee47160665c685
Dan Moran <scrumassociates@gmail.com>
2020-03-25 23:42:52 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,75 @@
+<script>
+  import Input from "./components/Input.svelte";
+  let configInputText = {
+    type: "number",
+    id: "firstName",
+    name: "firstName",
+    placeholder: "first name",
+    label: "First Name",
+    hint: "There's a problem here.",
+    min: 0,
+    max: 24
+  };
+  let value = "Alvin";
+</script>
+
+<style>
+  h1 {
+    color: var(--theme-alert-info-fg);
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  .alert-info,
+  .alert-success,
+  .alert-warning,
+  .alert-error {
+    --margin: 8px;
+    --padding: 24px;
+    border: 0;
+    border-radius: 4px;
+    width: calc(100%- (2 * var(--margin)));
+    margin: var(--margin);
+    padding: var(--padding);
+  }
+  .alert-info {
+    color: var(--theme-alert-info-fg);
+    background-color: var(--theme-alert-info-bg);
+  }
+  .alert-success {
+    color: var(--theme-alert-success-fg);
+    background-color: var(--theme-alert-success-bg);
+  }
+  .alert-warning {
+    color: var(--theme-alert-warning-fg);
+    background-color: var(--theme-alert-warning-bg);
+  }
+  .alert-error {
+    color: var(--theme-alert-error-fg);
+    background-color: var(--theme-alert-error-bg);
+  }
+  main {
+    margin: 4px;
+    padding: 0px;
+    border: 1px solid black;
+  }
+  .container {
+    width: 100%;
+    margin: 0;
+    padding: 8px;
+  }
+</style>
+
+<main>
+  <div class="container">
+    <h4>{value}&nbsp;</h4>
+
+    <Input config={configInputText} bind:value />
+
+  </div>
+  <div class="alert-info">This is an info alert.</div>
+  <div class="alert-success">This is a success alert.</div>
+  <div class="alert-warning">This is a warning alert.</div>
+  <div class="alert-error">This is an error alert.</div>
+</main>
---
+++ src/components/Input.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let alt = "";
+  export let autocomplete = "off";
+  export let autofocus = "";
+  export let disabled = "";
+  export let id = "id";
+  export let max = undefined;
+  export let maxlength = undefined;
+  export let min = undefined;
+  export let minlength = undefined;
+  export let name = undefined;
+  export let placeholder = "placeholder";
+  export let readonly = false;
+  export let required = false;
+  export let step = undefined;
+  export let type = "text"; // date, email, number, password, tel, text
+  export let value = "";
+
+  function handler(e) {
+    value = e.target.value;
+  }
+</script>
+
+<style>
+
+</style>
+
+<div class="input-group">
+  <label for={config.id}>{config.label}</label>
+  <input {alt} {autocomplete} on:change|preventDefault={handler} {value} />
+  <div class="input-hint">{config.hint}&nbsp;</div>
+</div>
